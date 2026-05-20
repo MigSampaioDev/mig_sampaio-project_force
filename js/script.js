1d8acd7509be10d193a889b511857fab839bcc94
Miguel Sampaio <miguelsam369@gmail.com>
2026-05-20 18:39:45 -0300

    Arquivos e pastas do projeto

---
+++ js/script.js
@@ -0,0 +1,53 @@
+function calcular() {
+
+    let respostas = 
+    [
+        document.getElementById("quest_1").value,
+        document.getElementById("quest_2").value,
+        document.getElementById("quest_3").value,
+        document.getElementById("quest_4").value,
+        document.getElementById("quest_5").value
+    ];
+
+    let sabres = 
+    {
+        red: 0,
+        blue: 0,
+        green: 0
+    };
+
+    respostas.forEach(resposta => 
+    {
+        // Sabre Vermelho (SITH)
+        if (resposta == "Muito Pouco" || resposta == "Pouco")
+        {
+            sabres.red++;
+        }
+
+        // Sabre Azul (JEDI)
+        if (resposta == "Muito" || resposta == "Ás vezes")
+        {
+            sabres.blue++;
+        }
+
+        // Sabre Verde (JEDI)
+        if (resposta == "Ás vezes")
+        {
+            sabres.green++;
+        }
+    });
+
+    let result = "AZUL";
+
+    if (sabres.red > sabres[result])
+    {
+        result = "VERMELHO";
+    }
+
+    if (sabres.green > sabres[result])
+    {
+        result = "VERDE";
+    }
+
+    console.log("Seu sabre é:", result);
+}
